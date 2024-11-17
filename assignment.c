c317b6b380a26d447be0c0a540b64bdedd228465
xDGeo <tgeo04@gmail.com>
2024-11-17 16:41:26 +0100

    bug fix matrix 1x1, try 2

--- assignment.c
+++ assignment.c
@@ -6,8 +6,8 @@
 #include <stdlib.h>
 
 
-void print_usage(int argc){
-    printf("%d arguments provided. You need to input exactly 2\n", argc-1);
+void print_usage(int argc) {
+    printf("Incorrect usage. You provided %d arguments. The correct number of arguments is 2\n", argc - 1);
 }
 
 void print_invalid_params() {
@@ -27,9 +27,6 @@ int is_positive_integer(const char *str) {
     return 1;
 }
 
-
-
-
 int main(int argc, char *argv[]) {
     // initialize random number generator
     srand(time(NULL));
@@ -40,7 +37,7 @@ int main(int argc, char *argv[]) {
         print_usage(argc);
         return 1;
     }
-    
+
     if (!is_positive_integer(argv[1]) || !is_positive_integer(argv[2])) {
         print_invalid_params();
         return 1;
@@ -54,13 +51,11 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-
     if (rows == 1 && cols == 1) {
         print_invalid_dimensions();
         return 1;
     }
 
-    
     int **matrix = malloc(rows * sizeof(int *));
     for (int i = 0; i < rows; i++) {
         matrix[i] = malloc(cols * sizeof(int));
@@ -74,10 +69,9 @@ int main(int argc, char *argv[]) {
 
     FILE *f = fopen("matrix.txt", "w");
     if (!f) {
-        fprintf(stderr, "writing of file failed\n");
+        fprintf(stderr, "Failed to open the file for writing\n");
         return 1;
     }
-
 
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
